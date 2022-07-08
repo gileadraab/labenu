1aa3bdd18d82410e76cc2f931dbfce79291fb3bd
Gilead Raab <gileadraab@gmail.com>
2022-07-08 20:59:57 -0300

    Parte logica quase finalizada, faltando apenas a pagina de resetar perfis

--- modulo3/projeto-astromatch/src/Components/Matches.js
+++ modulo3/projeto-astromatch/src/Components/Matches.js
@@ -3,10 +3,30 @@ import { useEffect, useState } from "react";
 import React from 'react'
 
 export default function Matches(props) {
+  const [matches, setMatches] = useState([])
+
+  useEffect(() => {
+    getMatches()
+  }, [])
+
+  const getMatches = (() => {
+    axios
+    .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gileadraab/matches")
+    .then((res) => setMatches(res.data.matches))
+    .catch((err) => alert(err.response))
+  })
+
+  console.log(matches)
+  const showMatches = matches.map(match => {
+    return <div><img src= {match.photo}height='50px' width='50px'/>{match.name}</div>
+  })
+
+
   return (
     <div>
       <button onClick={() => props.changeScreen("profiles")}>Ir para Profiles</button>
       <div>Matches</div>
+      {showMatches}
     </div>
   )
 }
--- modulo3/projeto-astromatch/src/Components/Profiles.js
+++ modulo3/projeto-astromatch/src/Components/Profiles.js
@@ -5,27 +5,52 @@ import React from 'react'
 export default function Profiles(props) {
   const [profile, setProfile] = useState({})
 
+  //Call the function getProfile every time the page is rendered
   useEffect(() => {
     getProfile()
   }, [])
 
-  const getProfile = (()=>{
+  //Get an random profile from the API
+  const getProfile = (() => {
     axios
     .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gileadraab/person")
     .then((res) => setProfile(res.data.profile))
     .catch((err) => alert(err.response))
+  })
+
+  //Check for the   between the user
+  const choosePerson = ((choice) => {
+    const match = {
+      id:profile.id,
+      choice: choice 
     }
-  )
+    axios
+    .post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gileadraab/choose-person", match)
+    .then ((response) => {
+      console.log(response)
+      response.data.isMatch  === true && alert("IT'S A MATCH")
+      getProfile()
+    })
+    .catch((err) => alert(err.response))
+  })
+
+  const resetProfiles = (() => {
+    axios
+    .put("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gileadraab/clear")
+    .then(getProfile())
+    .catch((err) => alert(err.response))
+  })
 
   return (
     <div>
       <button onClick={() => props.changeScreen("matches")}>Ir para Matches</button>
       <div>Profiles</div>
-      <img src={profile.photo}></img>
+      <img src={profile.photo} height='200px' ></img>
       <p>Name: {profile.name}, {profile.age}</p> 
       <p>Bio: {profile.bio}</p>
-      <button onClick={() => props.changeScreen("matches")}>LIKE</button>
-      <button onClick={() => getProfile()}>DISLIKE</button>
+      <button onClick={() => getProfile(false)}>DISLIKE</button>
+      <button onClick={() => choosePerson(true)}>LIKE</button>
+      <div><button onClick={() => resetProfiles()}>RESETAR PERFIS</button></div>
 
 
     </div>
