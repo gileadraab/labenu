b6000cb3927e9f08c48134799c0fb9faa7fc8b72
Gilead Raab <gileadraab@gmail.com>
2022-09-19 22:59:08 -0300

    Exercícios Arquitetura-Camadas

---
+++ modulo5/arquitetura-camadas/src/controller/UserController.ts
@@ -0,0 +1,76 @@
+import { Request, Response } from "express"
+import { UserBusiness } from "../business/UserBusiness"
+
+export class UserController {
+  //SIGN UP
+  signup = async (req: Request, res: Response) => {
+    try{
+      const {name, email, password} = req.body
+
+      const input = {
+        name,
+        email,
+        password,
+      }
+
+      const userBusiness = new UserBusiness()
+      const response = await userBusiness.signup(input)
+
+      res.status(201).send(response)
+
+    }catch (error: any) {
+      res.status(400).send({message: error.message})
+    }
+  }
+
+  //LOGIN
+  login = async (req: Request, res: Response) => {
+    try{
+      const{email, password} = req.body
+
+      const input = {
+        email,
+        password
+      }
+
+      const userBusiness = new UserBusiness()
+      const response = await userBusiness.login(input)
+
+      res.status(201).send(response)
+
+    }catch (error: any){
+      res.status(500).send({message: error.message})
+    }
+  }
+
+  //SEARCH USERS
+  search = async (req: Request, res: Response) => {
+    try{
+      const token = req.headers.authorization as string
+
+      const userBusiness = new UserBusiness()
+      const response = await userBusiness.search(token)
+
+      res.status(201).send(response)
+
+    }catch (error: any){
+      res.status(500).send({message: error.message})
+    }
+  }
+
+  //DELETE USER
+  delete = async (req: Request, res: Response) => {
+    try{
+      const token = req.headers.authorization as string
+      const userId = req.params.id
+
+      const userBusiness = new UserBusiness()
+      const response = await userBusiness.delete(token, userId)
+
+      res.status(201).send(response)
+
+    }catch (error: any){
+      res.status(500).send({message: error.message})
+    }
+  }
+}
